782700f62e418cd93215a1ebbb28fc5a21ed4d50
Edii Knorand <edii@patchu.knowledge>
2012-08-18 16:41:27 -0300

    Added window functions

---
+++ src/gui/window.c
@@ -0,0 +1,70 @@
+#include <SDL/SDL.h>
+#include <stdlib.h>
+#include <string.h>
+#include "window.h"
+#include "theme.h"
+#include "../engine/render.h"
+
+/* - - - - */
+void guiInit()
+{
+  guiStack.top = NULL;
+}
+
+void guiClear()
+{
+  GUIWindow *w, *aux;
+
+  w = guiStack.top;
+  while(w)
+  {
+    free(w->field);
+    aux = w->caller;
+    free(w);
+    w = aux;
+  }
+}
+
+/* - - - - */
+GUIWindow *guiSummonWindow(char *title, SDL_Rect *wrect, int n_fields, GUITheme *theme)
+{
+  GUIWindow *w = malloc(sizeof(GUIWindow));
+
+  w->caller = guiStack.top;
+  guiStack.top = w;
+
+  memcpy(&(w->wrect), wrect, sizeof(SDL_Rect));
+  w->title = title;
+  w->field = malloc(n_fields*sizeof(GUIField));
+  w->fsize = n_fields;
+  w->filled = 0;
+
+  w->theme = theme;
+
+  return w;
+}
+
+void guiUnsummonWindow()
+{
+  GUIWindow *w;
+
+  if(!guiStack.top)
+    return;
+  w = guiStack.top;
+  free(w->field);
+  guiStack.top = w->caller;
+  free(w);
+}
+
+/* - - - - */
+void guiAddField(GUIWindow *w, SDL_Rect *frect, SDL_Surface *surface, SDL_Rect *src_rect)
+{
+  if(w->filled >= w->fsize)
+    return;
+  memcpy(&(w->field[w->filled].frect), frect, sizeof(SDL_Rect));
+  memcpy(&(w->field[w->filled].src_rect), src_rect, sizeof(SDL_Rect));
+  w->field[w->filled].surface = surface;
+  (w->filled)++;
+}
+
+/* - - - - */
---
+++ src/gui/window.h
@@ -0,0 +1,41 @@
+#ifndef H_GUI_CORE_H
+#define H_GUI_CORE_H
+
+#include <SDL/SDL.h>
+
+typedef struct
+{
+  SDL_Rect frect; // dest rect (screen)
+  SDL_Rect src_rect; // src rect (field surface)
+  SDL_Surface *surface;
+} GUIField;
+
+typedef struct _GUIWindow
+{
+  char *title;
+  SDL_Rect wrect;
+
+  GUIField *field;
+  Uint16 fsize;
+
+  struct _GUIWindow *caller;
+  GUITheme *theme;
+} GUIWindow;
+
+typedef struct
+{
+  GUIWindow *top;
+} GUIStack;
+
+GUIStack guiStack;
+
+/* Functions */
+void guiInit();
+void guiClear();
+
+GUIWindow *guiSummonWindow(char *title, SDL_Rect *wrect, int n_fields, GUITheme *theme);
+void guiUnsummonWindow();
+
+void guiAddField(GUIWindow *w, SDL_Rect *frect, SDL_Surface *surface, SDL_Rect *src_rect);
+
+#endif
